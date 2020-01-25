c99b525059093c764f58628cc463621293f17008
Sarvesh <sarveshkumar331@gmail.com>
2020-01-25 11:52:05 +0530

    first commit----v1

---
+++ palindrome_check_string.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+bool palindrome_check(char *str)
+{
+	char *head=str;
+	char *tail=str+strlen(str)-1;
+	while(head<tail)
+	{
+		if(*head!=*tail)
+		return false;
+		head++;
+		tail--;
+	}
+	return true;
+}
+
+int main()
+{
+	char ch[20];
+	cout<<"enter string for palindrom check::"<<endl;
+	gets(ch);
+	if(palindrome_check(ch))
+	cout<<ch<<" "<<"is palindrome"<<endl;
+	else 
+	cout<<ch<<" "<<"is not palindrome"<<endl;
+	return 0;
+}
+
